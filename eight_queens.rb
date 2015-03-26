2032d80ae097e73b7ad33da55e64e4267b5f5827
chalzstrassen <yuchao.137chen@gmail.com>
2015-03-25 23:50:02 -0400

    Implemented the diags, fix them

--- eight_queens.rb
+++ eight_queens.rb
@@ -1,7 +1,11 @@
 class Eightqueens
   Q = true
-  def initialize
-    @board = Array.new(8) { Array.new(8) }
+  def self.empty
+    Array.new(8) { Array.new(8) }
+
+  end
+  def initialize(board = self.class.empty)
+    @board = board 
     @valid_board = Array.new(8) { Array.new(8, true) }
   end
 
@@ -11,64 +15,11 @@ class Eightqueens
     @valid_board[row][col]
   end
 
-
-
-  def play
-    puts "Choose which row to place queen:"
-    row = gets.chomp.to_i
-    puts row
-    puts "Choose which column to place queen:"
-    col = gets.chomp.to_i
-    puts col
-    place_queen(row, col) if valid_spot?(row, col)
-
-    display_board
-    if solved?
-      puts "You win"
-    else
-      play
-    end
-
-    # query user again if game has not been won, play unless solved?
-    # no more moves left if all spots on board are ineligible, end game
-  end
-
   def place_queen(row, col)
     @board[row][col] = Q unless @board[row][col]
     invalidate_rows(row)
     invalidate_cols(col)
     invalidate_diags(row, col)
-  end
-
-  def invalidate_rows(row)
-    (0..7).each do |i|
-      @valid_board[row][i] = false
-    end
-
-  end
-
-  def invalidate_cols(col)
-    (0..7).each do |i|
-      @valid_board[i][col] = false
-    end
-
-  end
-
-  def invalidate_diags(row,col)
-    # go southeast [+1,+1]
-    invalidate_left_down(row, col)
-    invalidate_left_up(row, col)
-
-  end
-
-  def invalidate_left_down(row, col)
-    unless row == 7
-      ((row+1)..7).each do |row|
-        col+=1
-        @valid_board[row][col] = false if col <= 7
-      end
-    end
-
   end
 
   def display_board
@@ -78,6 +29,10 @@ class Eightqueens
         col ? "Q" : " "
       end
     end.each do |row|
+      p row
+    end
+
+    @valid_board.each do |row|
       p row
     end
 
@@ -94,4 +49,104 @@ class Eightqueens
     true
   end
 
+  def no_valid?
+    invalid_board = Array.new(8){Array.new(8, false)}
+    @valid_board == 
+
+  end
+
+  protected
+  def invalidate_rows(row)
+    (0..7).each do |i|
+      @valid_board[row][i] = false
+    end
+
+  end
+
+  def invalidate_cols(col)
+    (0..7).each do |i|
+      @valid_board[i][col] = false
+    end
+  end
+
+  def invalidate_diags(row,col)
+    # go southeast [+1,+1]
+    invalidate_left_down(row, col)
+    invalidate_left_up(row, col)
+    invalidate_right_up(row,col)
+    invalidate_right_down(row,col)
+  end
+
+  def invalidate_left_down(row, col)
+    unless row == 7 || 
+      ((row+1)..7).each do |row|
+        col -= 1 
+        @valid_board[row][col] = false if col >= 0
+      end
+    end
+  end
+
+  def invalidate_left_up(row, col)
+    unless row == 0 
+      (row-1).downto(0).each do |row|
+        col -= 1
+        @valid_board[row][col] = false if col >= 0
+      end
+    end
+  end
+
+  def invalidate_right_up(row, col)
+    unless row == 0 
+      (row-1).downto(0).each do |row|
+        col += 1
+        @valid_board[row][col] = false if col <= 0
+      end
+    end
+  end
+
+  def invalidate_right_down(row, col)
+    unless row == 7 
+      ((row+1)..7).each do |row|
+        col += 1 
+        @valid_board[row][col] = false if col <= 0
+      end
+    end
+  end
+
+  
+
 end
+
+class Solver
+  def initialize
+    @puzzle = Eightqueens.new
+  end
+
+  def play
+    user_interact
+
+    @puzzle.display_board
+    if @puzzle.solved?
+      puts "You win!"
+    elsif @puzzle.no_valid?
+      puts "No more valid moves. You lose!"
+    else
+      play
+    end
+  end
+
+  def user_interact
+    puts "Choose which row to place queen:"
+    row = gets.chomp.to_i
+    puts row
+    puts "Choose which column to place queen:"
+    col = gets.chomp.to_i
+    puts col
+    @puzzle.place_queen(row, col) if @puzzle.valid_spot?(row, col)
+  end
+end
+
+if __FILE__ == $PROGRAM_NAME
+  Solver.new.play
+end
+
